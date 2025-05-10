cc5684dcf4f82eba06970006fbe248353100c903
Fabiovgc <fabiovgc@gmail.com>
2025-05-09 22:59:25 -0300

    initial commit - Vite and React project

---
+++ src/App2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+// rafce
+
+// const App2 = () => <h1> Hello World </h1>;
+
+  
+// export default App2.jsx;
